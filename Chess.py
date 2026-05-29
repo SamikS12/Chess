3b04dfcc54b6f8860418a7e9db09cb84a36bb95d
mabouelleil2027-dev <mabouelleil2027@student.amsacs.org>
2026-05-29 13:23:55 -0400

    Fixed pieces

--- Chess.py
+++ Chess.py
@@ -10,19 +10,19 @@ screen = pygame.display.set_mode((900, 750), pygame.RESIZABLE)
 
 board = pygame.image.load("Chess_Board/brown.png").convert_alpha()
 
-bp = pygame.image.load("Chess_Pieces/bk.png").convert_alpha() #black pawn
-bn = pygame.image.load("Chess_Pieces/bb.png").convert_alpha() #black knight
+bp = pygame.image.load("Chess_Pieces/bp.png").convert_alpha() #black pawn
+bn = pygame.image.load("Chess_Pieces/bn.png").convert_alpha() #black knight
 bb = pygame.image.load("Chess_Pieces/bb.png").convert_alpha() #black bishop
-br = pygame.image.load("Chess_Pieces/bk.png").convert_alpha() #black rook  
-bq = pygame.image.load("Chess_Pieces/bb.png").convert_alpha() #black queen
+br = pygame.image.load("Chess_Pieces/br.png").convert_alpha() #black rook  
+bq = pygame.image.load("Chess_Pieces/bq.png").convert_alpha() #black queen
 bk = pygame.image.load("Chess_Pieces/bk.png").convert_alpha() #black king  
 
-wp = pygame.image.load("Chess_Pieces/bk.png").convert_alpha() #white pawn
-wn = pygame.image.load("Chess_Pieces/bb.png").convert_alpha() #white knight
-wb = pygame.image.load("Chess_Pieces/bb.png").convert_alpha() #white bishop
-wr = pygame.image.load("Chess_Pieces/bk.png").convert_alpha() #white rook  
-wq = pygame.image.load("Chess_Pieces/bb.png").convert_alpha() #white queen
-wk = pygame.image.load("Chess_Pieces/bk.png").convert_alpha() #white king  
+wp = pygame.image.load("Chess_Pieces/wp.png").convert_alpha() #white pawn
+wn = pygame.image.load("Chess_Pieces/wn.png").convert_alpha() #white knight
+wb = pygame.image.load("Chess_Pieces/wb.png").convert_alpha() #white bishop
+wr = pygame.image.load("Chess_Pieces/wr.png").convert_alpha() #white rook  
+wq = pygame.image.load("Chess_Pieces/wq.png").convert_alpha() #white queen
+wk = pygame.image.load("Chess_Pieces/wk.png").convert_alpha() #white king  
 
 running = True
 while running:
