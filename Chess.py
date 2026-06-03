24684ef52cba40300c45417db4be50ca143b068e
mabouelleil2027-dev <mabouelleil2027@student.amsacs.org>
2026-06-03 10:20:18 -0400

    Added DrawPieces method

--- Chess.py
+++ Chess.py
@@ -1,52 +1,86 @@
+#Chess.py
 import pygame
-import time
-from datetime import datetime
-from sys import platform
-import os
-from copy import deepcopy
-from ChessState import ChessState
+from ChessState import ChessState, Piece
 
-def main():
-    pygame.init()
-    screen = pygame.display.set_mode((750, 750), pygame.RESIZABLE)
-    pygame.display.set_caption("Chess Game")
+pygame.init()
+screen = pygame.display.set_mode((750, 750), pygame.RESIZABLE)
+pygame.display.set_caption("Chess")
 
-    board = pygame.image.load("Chess_Board/brown.png").convert_alpha()
-    scaled_board = pygame.transform.scale(board, (750, 750))
+boardImg = pygame.image.load("Chess_Board/brown.png").convert_alpha()
 
-    #PIECES BLACK & WHITE
-    bp = pygame.image.load("Chess_Pieces/bp.png").convert_alpha() #black pawn
-    bn = pygame.image.load("Chess_Pieces/bn.png").convert_alpha() #black knight
-    bb = pygame.image.load("Chess_Pieces/bb.png").convert_alpha() #black bishop
-    br = pygame.image.load("Chess_Pieces/br.png").convert_alpha() #black rook  
-    bq = pygame.image.load("Chess_Pieces/bq.png").convert_alpha() #black queen
-    bk = pygame.image.load("Chess_Pieces/bk.png").convert_alpha() #black king  
+#PIECES BLACK & WHITE
+pieceImages = {
+    "bp": pygame.image.load("Chess_Pieces/bp.png").convert_alpha(), #black pawn
+    "bn": pygame.image.load("Chess_Pieces/bn.png").convert_alpha(), #black knight
+    "bb": pygame.image.load("Chess_Pieces/bb.png").convert_alpha(), #black bishop
+    "br": pygame.image.load("Chess_Pieces/br.png").convert_alpha(), #black rook
+    "bq": pygame.image.load("Chess_Pieces/bq.png").convert_alpha(), #black queen
+    "bk": pygame.image.load("Chess_Pieces/bk.png").convert_alpha(), #black king
+    "wp": pygame.image.load("Chess_Pieces/wp.png").convert_alpha(), #white pawn
+    "wn": pygame.image.load("Chess_Pieces/wn.png").convert_alpha(), #white knight
+    "wb": pygame.image.load("Chess_Pieces/wb.png").convert_alpha(), #white bishop
+    "wr": pygame.image.load("Chess_Pieces/wr.png").convert_alpha(), #white rook
+    "wq": pygame.image.load("Chess_Pieces/wq.png").convert_alpha(), #white queen
+    "wk": pygame.image.load("Chess_Pieces/wk.png").convert_alpha(), #white king
+}
 
-    wp = pygame.image.load("Chess_Pieces/wp.png").convert_alpha() #white pawn
-    wn = pygame.image.load("Chess_Pieces/wn.png").convert_alpha() #white knight
-    wb = pygame.image.load("Chess_Pieces/wb.png").convert_alpha() #white bishop 
-    wr = pygame.image.load("Chess_Pieces/wr.png").convert_alpha() #white rook  
-    wq = pygame.image.load("Chess_Pieces/wq.png").convert_alpha() #white queen
-    wk = pygame.image.load("Chess_Pieces/wk.png").convert_alpha() #white king  
+BOARD_SIZE = 750
+SQUARE_SIZE = BOARD_SIZE // 8
 
-    clock = pygame.time.Clock()
-    running = True
-    
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                clicked_row, clicked_col = pygame.mouse.get_pos()
-                print(f"Clicked square: Row {clicked_row}, Column {clicked_col}")
-                move = [clicked_row, clicked_col]
+game = ChessState()
+selectedSquare = None
+validMoves = []
 
-        screen.fill((255, 255, 255))
-        screen.blit(scaled_board, (0, 0))
-        pygame.display.flip() 
-        clock.tick(60)
+def pixelToSquare(x, y):
+    col = x // SQUARE_SIZE
+    row = y // SQUARE_SIZE
+    return row, col
 
-    pygame.quit()
+def drawBoard():
+    scaledBoard = pygame.transform.scale(boardImg, (BOARD_SIZE, BOARD_SIZE))
+    screen.blit(scaledBoard, (0, 0))
 
-if __name__ == "__main__":
-    main()
+def drawPieces():
+    for row in range(8):
+        for col in range(8):
+            pieceCode = game.board[row][col]
+            if pieceCode and pieceCode in pieceImages:
+                img = pygame.transform.scale(pieceImages[pieceCode], (SQUARE_SIZE, SQUARE_SIZE))
+                screen.blit(img, (col * SQUARE_SIZE, row * SQUARE_SIZE))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouseX, mouseY = pygame.mouse.get_pos()
+            if mouseX < BOARD_SIZE and mouseY < BOARD_SIZE:
+                clickedSquare = pixelToSquare(mouseX, mouseY)
+                row, col = clickedSquare
+                pieceCode = game.board[row][col]
+
+                if selectedSquare is None:
+                    if pieceCode:
+                        selectedSquare = clickedSquare
+                        piece = Piece(pieceCode, clickedSquare)
+                        validMoves = piece.getValidPawnMove(game.board)
+                else:
+                    if clickedSquare in validMoves:
+                        sr, sc = selectedSquare
+                        game.board[row][col] = game.board[sr][sc]
+                        game.board[sr][sc] = ""
+                        selectedSquare = None
+                        validMoves = []
+                    elif clickedSquare == selectedSquare:
+                        selectedSquare = None
+                        validMoves = []
+
+    screen.fill((255, 255, 255))
+    drawBoard()
+    drawPieces()
+    pygame.display.flip()
+
+pygame.quit()
+
--- ChessState.py
+++ ChessState.py
@@ -1,83 +1,45 @@
-#from enum import Enum
-import pygame
-import sys 
-
-
-#PIECES BLACK & WHITE
-# bp = pygame.image.load("Chess_Pieces/bp.png").convert_alpha() #black pawn
-# bn = pygame.image.load("Chess_Pieces/bn.png").convert_alpha() #black knight
-# bb = pygame.image.load("Chess_Pieces/bb.png").convert_alpha() #black bishop
-# br = pygame.image.load("Chess_Pieces/br.png").convert_alpha() #black rook  
-# bq = pygame.image.load("Chess_Pieces/bq.png").convert_alpha() #black queen
-# bk = pygame.image.load("Chess_Pieces/bk.png").convert_alpha() #black king  
-
-# wp = pygame.image.load("Chess_Pieces/wp.png").convert_alpha() #white pawn
-# wn = pygame.image.load("Chess_Pieces/wn.png").convert_alpha() #white knight
-# wb = pygame.image.load("Chess_Pieces/wb.png").convert_alpha() #white bishop
-# wr = pygame.image.load("Chess_Pieces/wr.png").convert_alpha() #white rook  
-# wq = pygame.image.load("Chess_Pieces/wq.png").convert_alpha() #white queen
-# wk = pygame.image.load("Chess_Pieces/wk.png").convert_alpha() #white king  
-
-
+#ChessState.py
 class ChessState:
     def __init__(self):
-        self.whiteToMove = True
         self.board = [
             ["br", "bn", "bb", "bq", "bk", "bb", "bn", "br"],
             ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
-            ["", "", "", "", "", "", "", ""],
-            ["", "", "", "", "", "", "", ""],
-            ["", "", "", "", "", "", "", ""],
-            ["", "", "", "", "", "", "", ""],
+            ["",   "",   "",   "",   "",   "",   "",   ""],
+            ["",   "",   "",   "",   "",   "",   "",   ""],
+            ["",   "",   "",   "",   "",   "",   "",   ""],
+            ["",   "",   "",   "",   "",   "",   "",   ""],
             ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
-            ["wr", "wn", "wb", "wq", "wk", "wb", "wn", "wr"]]
+            ["wr", "wn", "wb", "wq", "wk", "wb", "wn", "wr"],
+        ]
 
-        self.moveLog = [] 
+class Piece:
+    def __init__(self, pieceCode, position):
+        self.pieceCode = pieceCode
+        self.color = pieceCode[0]
+        self.type = pieceCode[1]
+        self.position = position
 
-    def main(self): 
-        printBoard()
+    def getValidPawnMove(self, board):
+        moves = []
+        return moves 
 
-    if __name__ == "__main__":
-        main()
+    def getValidKnightMove(self, board):
+        moves = []
+        return moves
 
-    def getValidPawnMove(self):
-        return validMoves
+    def getValidBishopMove(self, board):
+        moves = []
+        return moves
 
-    def getValidKnightMove(self):
-        return validMoves
+    def getValidRookMove(self, board):
+        moves = []
+        return moves
 
-    def getValidBishopMove(self):
-        return validMoves
+    def getValidQueenMove(self, board):
+        moves = []
+        return moves
 
-    def getValidRookMove(self):
-        return validMoves
+    def getValidKingMove(self, board):
+        moves = []
+        return moves
 
-    def getValidQueenMove(self):
-        return validMoves
-
-    def getValidKingMove(self):
-        return validMoves
-        
-    def makeMove(self, startRow, startCol, endRow, endCol):
-
-        pieceMoved = self.board[startRow][startCol]
-
-        if pieceMoved == "":
-            return False
-
-        self.board[endRow][endCol] = pieceMoved
-        self.board[startRow][startCol] = ""
-
-        self.moveLog.append(
-            ((startRow, startCol), (endRow, endCol), pieceMoved))
-
-        self.whiteToMove = not self.whiteToMove
-
-        return True
-
-    def getPiece(self, row, col):
-        return self.board[row][col]
-
-    def printBoard(self):
-        for row in self.board:
-            print(row)
