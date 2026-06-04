22a3550b386fddd78ef141288c67596cfb84f973
SamikS12 <sshah2027@student.amsacs.org>
2026-06-04 09:58:22 -0400

    Added getCord for all validMoves

--- ChessState.py
+++ ChessState.py
@@ -37,21 +37,81 @@ class Piece:
 
     def getValidKnightMove(self, board):
         moves = []
-        return moves
+        whiteKnightCords = []
+        blackKnightCords = []
+
+        for row in range(len(board)):
+            for col in range(len(board[row])):
+                if board[row][col] == "bn":
+                    blackKnightCords.append((row, col))
+                    print(f"Black knight on:{blackKnightCords}")
+                if board[row][col] == "wn":
+                    whiteKnightCords.append((row, col))
+                    print(f"White knight on:{whiteKnightCords}")
+
+        return moves 
 
     def getValidBishopMove(self, board):
         moves = []
-        return moves
+        whiteBishopCords = []
+        blackBishopCords = []
+
+        for row in range(len(board)):
+            for col in range(len(board[row])):
+                if board[row][col] == "bb":
+                    blackBishopCords.append((row, col))
+                    print(f"Black bishop on:{blackBishopCords}")
+                if board[row][col] == "wb":
+                    whiteBishopCords.append((row, col))
+                    print(f"White bishop on:{whiteBishopCords}")
+
+        return moves 
 
     def getValidRookMove(self, board):
         moves = []
-        return moves
+        whiteRookCords = []
+        blackRookCords = []
+
+        for row in range(len(board)):
+            for col in range(len(board[row])):
+                if board[row][col] == "br":
+                    blackRookCords.append((row, col))
+                    print(f"Black rook on:{blackRookCords}")
+                if board[row][col] == "wr":
+                    whiteRookCords.append((row, col))
+                    print(f"White rook on:{whiteRookCords}")
+
+        return moves 
 
     def getValidQueenMove(self, board):
         moves = []
+        whiteQueenCords = []
+        blackQueenCords = []
+
+        for row in range(len(board)):
+            for col in range(len(board[row])):
+                if board[row][col] == "bq":
+                    blackQueenCords.append((row, col))
+                    print(f"Black queen on:{blackQueenCords}")
+                if board[row][col] == "wq":
+                    whiteQueenCords.append((row, col))
+                    print(f"White queen on:{whiteQueenCords}")
+
         return moves
 
     def getValidKingMove(self, board):
         moves = []
+        whiteKingCords = []
+        blackKingCords = []
+
+        for row in range(len(board)):
+            for col in range(len(board[row])):
+                if board[row][col] == "bk":
+                    blackKingCords.append((row, col))
+                    print(f"Black king on:{blackKingCords}")
+                if board[row][col] == "wk":
+                    whiteKingCords.append((row, col))
+                    print(f"White king on:{whiteKingCords}")
+
         return moves
 
