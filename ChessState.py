b5332d51c608a7dd424141cb665c5e81d49eb236
SamikS12 <sshah2027@student.amsacs.org>
2026-06-08 08:36:51 -0400

    did validBishopMoves

--- ChessState.py
+++ ChessState.py
@@ -42,6 +42,32 @@ class Piece:
         return []
 
     def getValidBishopMove(self, board):
+        moves = []
+        row, col = self.position
+
+        directions = [(-1, 1),  (1, 1),   (1, -1),  (-1, -1)]
+
+        for dRow, dCol in directions:
+            r = row + dRow
+            c = col + dCol
+
+            while 0 <= r < 8 and 0 <= c < 8:
+                piece = board[r][c]
+
+                if piece == "":
+                    moves.append((r, c))
+
+                elif piece[0] != self.color:
+                    moves.append((r, c))
+                    break
+
+                else:
+                    break
+
+                r += dRow
+                c += dCol
+
+        return moves
         return []
 
     def getValidRookMove(self, board):
@@ -79,7 +105,7 @@ class Piece:
     def getValidKingMove(self, board):
         moves = []
         x, y = self.position
-        curColor = board[x,y]
+        curColor = board[x][y]
         change = [
             (-1,-1), (-1,0), (-1,1), (0,-1), (0,1), (1,0), (1,1), (1,-1)
         ]
@@ -93,7 +119,6 @@ class Piece:
                 
                 if newPos == "":
                     moves.append((newX, newY))
-                if (newPos[0] != curColor[0]):
-                    moves.append((newX, newY))
+                
         return moves
 
